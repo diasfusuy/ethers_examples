5f15d4af555033394bd14bc02e83bb5c5b6e4d56
diasfusuy <diasfusuy4@gmail.com>
2025-03-24 15:14:20 -0500

    How to read smart contract in ether.js

--- examples/2_read_smart_contract.js
+++ examples/2_read_smart_contract.js
@@ -1,32 +1,29 @@
 const { ethers } = require("ethers");
 
-const INFURA_ID = ''
-const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
+const provider = new ethers.providers.JsonRpcProvider(`https://polygon-mainnet.infura.io/v3/91cfa7ea066146ffbf977b0b531840ad`);
 
 const ERC20_ABI = [
     "function name() view returns (string)",
     "function symbol() view returns (string)",
     "function totalSupply() view returns (uint256)",
     "function balanceOf(address) view returns (uint)",
-];
+]
 
-const address = '0x6B175474E89094C44Da98b954EedeAC495271d0F' // DAI Contract
-const contract = new ethers.Contract(address, ERC20_ABI, provider)
+const address = "0x2C89bbc92BD86F8075d1DEcc58C7F4E0107f286b";
 
-const main = async () => {
+const contract = new ethers.Contract(address, ERC20_ABI, provider);
+
+const main = async() => {
     const name = await contract.name()
     const symbol = await contract.symbol()
     const totalSupply = await contract.totalSupply()
-
-    console.log(`\nReading from ${address}\n`)
-    console.log(`Name: ${name}`)
-    console.log(`Symbol: ${symbol}`)
+    console.log("name:", name)
+    console.log("symbol:", symbol)
     console.log(`Total Supply: ${totalSupply}\n`)
 
-    const balance = await contract.balanceOf('0x6c6Bc977E13Df9b0de53b251522280BB72383700')
-
-    console.log(`Balance Returned: ${balance}`)
-    console.log(`Balance Formatted: ${ethers.utils.formatEther(balance)}\n`)
-}
+    const balance = await contract.balanceOf("0x2C89bbc92BD86F8075d1DEcc58C7F4E0107f286b")
+    console.log(`Balance returned: ${balance}`)
+    console.log(`Balance formatted: ${ethers.utils.formatEther(balance)}\n`)
+};
 
 main()
