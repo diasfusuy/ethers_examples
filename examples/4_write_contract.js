f7c26dd5d921c595050d6eaa9eb7f90e7c7ae9e9
diasfusuy <diasfusuy4@gmail.com>
2025-03-25 00:57:43 -0500

    Write contracts with ether.js

--- examples/4_write_contract.js
+++ examples/4_write_contract.js
@@ -1,30 +1,31 @@
 const { ethers } = require("ethers");
+const { isAddress } = require("ethers/lib/utils");
 
-const INFURA_ID = ''
-const provider = new ethers.providers.JsonRpcProvider(`https://kovan.infura.io/v3/${INFURA_ID}`)
+const provider = new ethers.providers.JsonRpcProvider(`https://polygon-mainnet.infura.io/v3/91cfa7ea066146ffbf977b0b531840ad`);
 
-const account1 = '' // Your account address 1
-const account2 = '' // Your account address 2
+const account1 = "0x9F48DE3fDB07b01b9F89c1241D493fcA1f5Fd464"
+const account2 = "0x5e74e6c8E6DBd753D20884ae16A2A99cF54d971F"
 
-const privateKey1 = '' // Private key of account 1
+const privateKey = "" //sender private key must be provided
 const wallet = new ethers.Wallet(privateKey1, provider)
 
-const ERC20_ABI = [
+const ERC_ABI = [
     "function balanceOf(address) view returns (uint)",
-    "function transfer(address to, uint amount) returns (bool)",
+    "function transfer(address, to, uint amount) returns bool()",
 ];
 
-const address = ''
-const contract = new ethers.Contract(address, ERC20_ABI, provider)
+const address = "" // token address
+const contract = new ethers.contract(address, ERC_ABI, provider)
 
-const main = async () => {
+const main = async() => {
+
     const balance = await contract.balanceOf(account1)
 
-    console.log(`\nReading from ${address}\n`)
+    console.log(`\nReading from ${address}`)
     console.log(`Balance of sender: ${balance}\n`)
 
-    const contractWithWallet = contract.connect(wallet)
-
+    const contractWithWallet = contract.conect(wallet)
+    
     const tx = await contractWithWallet.transfer(account2, balance)
     await tx.wait()
 
@@ -33,8 +34,8 @@ const main = async () => {
     const balanceOfSender = await contract.balanceOf(account1)
     const balanceOfReciever = await contract.balanceOf(account2)
 
-    console.log(`\nBalance of sender: ${balanceOfSender}`)
-    console.log(`Balance of reciever: ${balanceOfReciever}\n`)
+    console.log(`\nBalance sfo sender: ${balanceOf(balanceOfSender)}`)
+    console.log(`Balance of reciever: ${balanceOf(balanceOfReciever)}\n`)
 }
 
 main()
