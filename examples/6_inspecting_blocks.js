71b2560d391529c0dd20907ff1d7d86c99699709
diasfusuy <diasfusuy4@gmail.com>
2025-03-25 01:44:38 -0500

    Getting block information by inspecting blocks

--- examples/6_inspecting_blocks.js
+++ examples/6_inspecting_blocks.js
@@ -1,21 +1,15 @@
 const { ethers } = require("ethers");
 
-const INFURA_ID = ''
-const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
+const provider = new ethers.providers.JsonRpcProvider(`https://polygon-mainnet.infura.io/v3/91cfa7ea066146ffbf977b0b531840ad`);
 
 const main = async () => {
     const block = await provider.getBlockNumber()
 
-    console.log(`\nBlock Number: ${block}\n`)
+    console.log(`\nLatest block nummber: ${block}\n`)
 
     const blockInfo = await provider.getBlock(block)
 
     console.log(blockInfo)
-
-    const { transactions } = await provider.getBlockWithTransactions(block)
-
-    console.log(`\nLogging first transaction in block:\n`)
-    console.log(transactions[0])
 }
 
 main()
